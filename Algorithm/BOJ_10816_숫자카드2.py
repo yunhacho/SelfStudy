1a94db327c49af4dc373911b26d9c39bf96c1281
yunhacho <jyh_5530@naver.com>
2021-08-17 01:03:37 +0900

    counter search

--- Algorithm/BOJ_10816_숫자카드2.py
+++ Algorithm/BOJ_10816_숫자카드2.py
@@ -4,6 +4,4 @@ from collections import Counter
 if __name__=="__main__":
     N=int(input()); cards=Counter(map(int, stdin.readline().split()))
     M=int(input()); number=map(int, stdin.readline().split())
-    print(cards.elements())  
-    for n in number:
-        print(cards.get(n, 0), end=' ')
+    for n in number: print(cards.get(n, 0), end=' ')
