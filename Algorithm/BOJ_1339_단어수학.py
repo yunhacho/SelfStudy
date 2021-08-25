6350522bacde0e021d9b707c16f115a8438f9445
yunhacho <jyh_5530@naver.com>
2021-08-25 17:33:49 +0900

    구현

--- Algorithm/BOJ_1339_단어수학.py
+++ Algorithm/BOJ_1339_단어수학.py
@@ -1,13 +1,13 @@
-alpha={}; result=0; score=9
-for _ in range(int(input())):
-  idx=1
-  for w in input()[::-1]:
-    if w in alpha.keys(): alpha[w]+=idx
-    else: alpha[w]=idx
-    idx*=10
-  
-alphadic=sorted(list(alpha.items()), key=lambda x: -x[1])
-for alphabet, count in alphadic:
-  result+=count*score
-  score-=1
-print(result)
+import sys
+
+def solve_1339(words: list)->int:
+  alpha={}
+  for word in words: 
+    for i, w in enumerate(word[::-1]):
+      if w in alpha.keys(): alpha[w]+=10**i
+      else: alpha[w]=10**i
+  return sum(v*n for v,n in zip(sorted(alpha.values(), reverse=True), range(9,0, -1)))
+
+if __name__=="__main__":
+  n, *words = map(lambda x: x.rstrip(), sys.stdin.readlines())
+  print(solve_1339(words))
