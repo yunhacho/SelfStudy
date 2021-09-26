8ed2518d99101b134af242ea517a18a8080261ed
yunhacho <jyh_5530@naver.com>
2021-09-26 11:57:03 +0900

    문자열, hash

---
+++ Algorithm/Programmers/Programmers_전화번호목록.py
@@ -0,0 +1,6 @@
+from collections import Counter
+def solution(phoneBook):
+    phoneBook=sorted(phoneBook)
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1): return False
+    return True
