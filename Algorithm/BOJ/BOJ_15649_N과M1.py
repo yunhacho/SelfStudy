bb248b5477f3ee3ebffc111f564ecd2421117f62
yunhacho <jyh_5530@naver.com>
2021-10-03 17:26:57 +0900

    backtraking

---
+++ Algorithm/BOJ/BOJ_15649_N과M1.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+n,m=map(int, input().split())
+
+permutation=lambda n,m:print('\n'.join(list(map(' '.join,permutations(map(str, range(1,n+1)),m)))))
+
+visited=[False]*(n+1)
+def dfs(depth,sequence):
+    if depth==m:
+        print(*sequence, sep=' ')
+        return
+    for i in range(1,n+1):
+        if not visited[i]:
+            visited[i]=True
+            dfs(depth+1,sequence[:]+[i])
+            visited[i]=False
+dfs(0,[])
